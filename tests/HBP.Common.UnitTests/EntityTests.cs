f5f54c37b3b47eb6c0b22b3fbcb77d80a1f54f9b
s.goncharov <sergeygoncharov904@hotmail.com>
2026-07-27 23:01:02 +0700

    Базовая доменная Entity

---
+++ tests/HBP.Common.UnitTests/EntityTests.cs
@@ -0,0 +1,51 @@
+﻿using FluentAssertions;
+
+namespace HBP.Common.UnitTests;
+
+public class EntityTests
+{
+    [Fact]
+    public void SameIdEqualsReturnsTrue()
+    {
+        var id = Guid.NewGuid();
+        var a = new TestEntity { Id = id };
+        var b = new TestEntity { Id = id };
+
+        a.Equals(b).Should().BeTrue();
+        (a == b).Should().BeTrue();
+    }
+
+    [Fact]
+    public void NullEqualsReturnsFalse()
+    {
+        var a = new TestEntity { Id = Guid.NewGuid() };
+
+        a.Equals((object?)null).Should().BeFalse();
+        (a == (object?)null).Should().BeFalse();
+    }
+
+    [Fact]
+    public void SameIdReturnsEqualsHashCode()
+    {
+        var id = Guid.NewGuid();
+        var a = new TestEntity { Id = id };
+        var b = new TestEntity { Id = id };
+
+        a.GetHashCode().Should().Be(b.GetHashCode());
+    }
+
+    [Fact]
+    public void HashSetContainsEntity()
+    {
+        var id = Guid.NewGuid();
+        var a = new TestEntity { Id = id };
+        var set = new HashSet<TestEntity> { a };
+
+        set.Contains(a).Should().BeTrue();
+        set.Contains(new TestEntity { Id = id }).Should().BeTrue();
+    }
+
+    private sealed class TestEntity : Entity<Guid>
+    {
+    }
+}
